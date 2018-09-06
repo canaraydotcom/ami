dc92355cfef3769585c613d9b0a80e4713809a54
Marc-Sefan Cassola <maccesch@gmail.com>
2018-09-06 13:39:01 +0100

    accounted for x distortion along a curved slice, added thickness to curved slice

--- src/shaders/shaders.curved.uniform.js
+++ src/shaders/shaders.curved.uniform.js
@@ -5,48 +5,51 @@ import DataShadersUniform from './shaders.data.uniform';
  * @module shaders/data
  */
 export default class ShadersUniform {
-  static uniforms(){
-    const us = {
-      ...DataShadersUniform.uniforms(),
+	static uniforms() {
+		const us = DataShadersUniform.uniforms();
 
-      'uCurveCoordinates':{
-        type: 't',
-        value: [],
-        typeGLSL: 'sampler2D',
-      },
-      'uCurveLength': {
-        type: 'f',
-        value: 1.0,
-        typeGLSL: 'float'
-      },
-      'uCurveTangentUpAngles': {
-        type: 'v2v',
-        value: [
-          new THREE.Vector2(0, 0),
-          new THREE.Vector2(1, 0),
-					new THREE.Vector2(1, 0),
-					new THREE.Vector2(1, 0),
-					new THREE.Vector2(1, 0),
-					new THREE.Vector2(1, 0),
-					new THREE.Vector2(1, 0),
-					new THREE.Vector2(1, 0),
-        ],
-        length: 8,
-        typeGLSL: 'vec2',
-      },
-      'uCurveTangentVectors': {
-				type: 't',
-				value: [],
-				typeGLSL: 'sampler2D',
-      },
-      'uCurvePlaneNormal': {
-        type: 'v3',
-        value: new THREE.Vector3(0, 0, 1),
-        typeGLSL: 'vec3',
-      }
-    };
-    delete us.uStep;
+		us['uCurveCoordinates'] = {
+			type: 't',
+			value: [],
+			typeGLSL: 'sampler2D',
+		};
 
-    return us;
-  }
+		us['uCurveLength'] = {
+			type: 'f',
+			value: 1.0,
+			typeGLSL: 'float'
+		};
+
+		us['uCurveTangentUpAngles'] = {
+			type: 'v2v',
+			value: [
+				new THREE.Vector2(0, 0),
+				new THREE.Vector2(1, 0),
+				new THREE.Vector2(1, 0),
+				new THREE.Vector2(1, 0),
+				new THREE.Vector2(1, 0),
+				new THREE.Vector2(1, 0),
+				new THREE.Vector2(1, 0),
+				new THREE.Vector2(1, 0),
+			],
+			length: 8,
+			typeGLSL: 'vec2',
+		};
+
+		us['uCurveTangentVectors'] = {
+			type: 't',
+			value: [],
+			typeGLSL: 'sampler2D',
+		};
+
+		us['uCurvePlaneNormal'] = {
+			type: 'v3',
+			value: new THREE.Vector3(0, 0, 1),
+			typeGLSL: 'vec3',
+		};
+
+		delete us.uStep;
+
+		return us;
+	}
 }
