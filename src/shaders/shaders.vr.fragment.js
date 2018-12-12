876b045e6fb99caa4608a3eb0231b0e6af53f5ba
Marc-Sefan Cassola <maccesch@gmail.com>
2018-12-12 14:02:42 +0000

    added noise to volume rendering to avoid aliasing artifacts

--- src/shaders/shaders.vr.fragment.js
+++ src/shaders/shaders.vr.fragment.js
@@ -47,6 +47,18 @@ export default class ShadersFragment {
     // language=GLSL
     this._main = `
 
+const float PI = 3.14159265358979323846264 * 00000.1; // PI
+
+// expects values in the range of [0,1]x[0,1], returns values in the [0,1] range.
+// do not collapse into a single function per: http://byteblacksmith.com/improvements-to-the-canonical-one-liner-glsl-rand-for-opengl-es-2-0/
+highp float rand( const in vec2 uv) {
+  const highp float a = 12.9898;
+  const highp float b = 78.233;
+  const highp float c = 43758.5453;
+  highp float dt = dot(uv.xy, vec2(a, b)), sn = mod(dt, PI);
+  return fract(sin(sn) * c);
+}
+
 float readDepth () {
     vec2 coord = vec2(
       (gl_FragCoord.x + uScreenOffsetX) / uScreenWidth, 
@@ -100,6 +112,10 @@ void main(void) {
     gl_FragColor.a = 0.0;
     return;
   }
+
+  float randomOffset = rand(gl_FragCoord.xy);
+  currentVoxel += stepVector * randomOffset;
+  currentZ += uStepSize * randomOffset;
   
   bool wasInside = false;
   
